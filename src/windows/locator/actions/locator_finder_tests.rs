8965ea643fa51bbf2374eff34d762033d1abf977
Viktor <40205218+ViktorKarpilov@users.noreply.github.com>
2025-03-07 10:42:07 +0200

    Implement rust pipeline

--- src/windows/locator/actions/locator_finder_tests.rs
+++ src/windows/locator/actions/locator_finder_tests.rs
@@ -3,6 +3,7 @@ mod tests {
     use crate::windows::locator::actions::locator_finder::get_root_locators;
 
     #[test]
+    #[ignore = "Only for local use"]
     fn test_add() {
         println!("Start locator");
         let locators = get_root_locators().ok().unwrap();
