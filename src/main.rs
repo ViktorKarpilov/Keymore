ac30c8ddad5cfeb4c5237be05b730813885f1e33
Viktor Karpilov <wktorion@gmail.com>
2025-03-07 10:45:48 +0200

    Comment out local code

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,16 @@ mod monitor;
 mod visual;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Start window");
-    thread::sleep(time::Duration::from_millis(5000));
-    let locators = get_root_locators()?;
-
-    let created = TransparentLayout::create_layout(locators.clone())?;
-
-    if let Some(choosen) = &created {
-        MouseOperator::click(choosen.physical_point);
-    }
-    println!("Choosen locator: {:?}", created);
+    // println!("Start window");
+    // thread::sleep(time::Duration::from_millis(5000));
+    // let locators = get_root_locators()?;
+    // 
+    // let created = TransparentLayout::create_layout(locators.clone())?;
+    // 
+    // if let Some(choosen) = &created {
+    //     MouseOperator::click(choosen.physical_point);
+    // }
+    // println!("Choosen locator: {:?}", created);
 
     Ok(())
 }
